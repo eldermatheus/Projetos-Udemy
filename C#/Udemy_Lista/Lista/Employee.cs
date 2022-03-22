9c91f21438698235a3cb3e2f21e9755f0c00890e
eldermatheus <elder_matheus11@hotmail.com>
2022-03-21 22:19:51 -0300

    Continuando curso Udemy

---
+++ C#/Udemy_Lista/Lista/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee() { 
+        }
+        public Employee( int id, string name, double salary)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Salary = salary;
+        }
+        public void IncreaseSalary(double percent) { 
+            Salary += Salary * percent / 100;
+        }
+
+        public override string ToString() { 
+            return  $"{Id}, {Name}, {Salary.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ C#/Udemy_Lista/Lista/Program.cs
@@ -0,0 +1,48 @@
+﻿using Lista;
+using System.Globalization;
+
+List<Employee> employees = new List<Employee>();
+
+Console.Write("How many employees will bee registered?");
+int employeesNumber = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= employeesNumber; i++)
+{
+    Console.WriteLine($"Employee #{i}:");
+        
+    Console.Write("Id: ");
+    int id = int.Parse(Console.ReadLine());
+    Console.Write("Name: ");
+    string name = Console.ReadLine();
+    Console.Write("Salary: ");
+    double salary = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+    employees.Add(new Employee(id, name, salary));
+    Console.WriteLine();
+}
+
+Console.Write("Enter the employee ud that will have salary increase : ");
+int selectEmployee = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the percentage: ");
+double percent = double.Parse(Console.ReadLine());
+
+
+Employee locatedEmployee = employees.Find(e => e.Id == selectEmployee);
+
+if (locatedEmployee != null)
+{
+    locatedEmployee.IncreaseSalary(percent);
+}
+
+else
+{
+    Console.WriteLine("This id does not exist!");
+}
+ 
+Console.WriteLine("Updated list of employess: ");
+
+foreach (var item in employees)
+{
+    Console.WriteLine(item);
+}
