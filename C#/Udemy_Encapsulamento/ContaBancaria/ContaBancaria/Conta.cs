1f07691e5779a43731b57845d9b2e9e346ddfa69
eldermatheus <elder_matheus11@hotmail.com>
2022-04-05 13:15:32 -0300

    Aplicando Encapsulamento

---
+++ C#/Udemy_Encapsulamento/ContaBancaria/ContaBancaria/Conta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria
+{
+    public class Conta
+    {
+        public int Numero { get; set; }
+        public string Titular { get; set; }
+        public decimal Saldo { get; set; }
+
+        public Conta() { 
+        }
+
+        public Conta(int numero, string titular)
+        {
+            this.Numero = numero;
+            this.Titular = titular;            
+        }        
+
+        public void Depositar(decimal valor) {
+            Saldo += valor;
+        }
+
+        public void Sacar(decimal valor)
+        {
+            Saldo -= valor;
+        }
+
+        public override string ToString() {
+            return $"Conta {Numero}, Titular: {Titular}, Saldo: $ {Saldo}";            
+        }
+    }
+}
---
+++ C#/Udemy_Encapsulamento/ContaBancaria/ContaBancaria/Program.cs
@@ -0,0 +1,36 @@
+﻿using ContaBancaria;
+
+Console.WriteLine("Entre o número da conta:");
+int numero = int.Parse(Console.ReadLine());
+Console.WriteLine("Entre o titular da conta:");
+string nome = Console.ReadLine();
+
+Conta conta = new Conta(numero, nome);
+
+Console.WriteLine("Haverá depósito inicial (s/n)?");
+string depositoInicial = Console.ReadLine().ToLower();
+
+
+if (depositoInicial.StartsWith('s')) {
+    
+    Console.WriteLine("Entre o valor de depósito inicial:");
+    decimal valorDepositoInicial = decimal.Parse(Console.ReadLine());    
+    conta.Saldo = valorDepositoInicial;
+}
+
+Console.WriteLine("Dados da conta:");
+Console.WriteLine(conta.ToString());
+
+//Operações
+
+Console.WriteLine("Entre um valor para depósito:");
+conta.Depositar(decimal.Parse(Console.ReadLine()));
+
+Console.WriteLine("Dados da conta atualizados:");
+Console.WriteLine(conta.ToString());
+
+Console.WriteLine("Entre um valor para saque:");
+conta.Sacar(decimal.Parse(Console.ReadLine()));
+
+Console.WriteLine("Dados da conta atualizados:");
+Console.WriteLine(conta.ToString());
